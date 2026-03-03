e9c9ad9655ccc03f59037681cedc433ba91761ab
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-03 14:46:55 +0100

    feat : add read all employees method

--- jour02/classe_employe.py
+++ jour02/classe_employe.py
@@ -7,6 +7,19 @@ class Employe :
         self.__database = access_database("job07")
         self.__cursor = point_cursor(self.__database)
 
+    def read_all_employees(self) : 
+
+        my_first_inner_join = """SELECT employe.nom, employe.prenom, service.nom, employe.id_service 
+                             FROM employe
+                             INNER JOIN service ON employe.id_service = service.id"""
+
+        self.__cursor.execute(my_first_inner_join)
+        results = self.__cursor.fetchall()
+
+        print("---------------- ALL EMPLOYEES AND THEIR SERVICE ----------------")
+        for nom, prenom, nom_service, service_id in results:
+            print(f"{prenom} {nom} travaille dans le service {nom_service} (service n°{service_id})")
+
     def create_employee(self) : 
         pass
 
@@ -21,3 +34,6 @@ class Employe :
         pass
     def edit_employee_service_id(self) : 
         pass
+
+employee_crud = Employe()
+employee_crud.read_all_employees()
